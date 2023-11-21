2df593dfafd6b21a879663861ca7cc67b3fb68f7
Nicolas Alayo <n_alayo_arias@outlook.com>
2023-11-21 11:10:16 -0500

    Conexion a BD

--- src/main/java/com/nibble/supermarket/Supermarket.java
+++ src/main/java/com/nibble/supermarket/Supermarket.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.nibble.supermarket;
 
-/**
- *
- * @author NICOLAS
- */
+import com.nibble.supermarket.servicios.PoblarBD;
+
 public class Supermarket {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        PoblarBD.poblarBD();
     }
 }
