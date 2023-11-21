e9f94b064c0f472f7207edcbf0f25b6f34cb8b6e
ArellyVega <arellyvegapeche@gmail.com>
2023-11-21 13:14:35 -0500

    Add logo images

--- src/main/java/com/nibble/supermarket/vista/Login.java
+++ src/main/java/com/nibble/supermarket/vista/Login.java
@@ -27,41 +27,143 @@ public class Login extends javax.swing.JFrame {
     private void initComponents() {
 
         background = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        nombreMarket = new javax.swing.JLabel();
+        imgLogin = new javax.swing.JLabel();
+        nibble = new javax.swing.JLabel();
+        loguito = new javax.swing.JLabel();
+        iniSesion = new javax.swing.JLabel();
+        contra = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        usuu = new javax.swing.JLabel();
+        ingresoUsu1 = new javax.swing.JTextField();
+        jSeparator2 = new javax.swing.JSeparator();
+        jPasswordField1 = new javax.swing.JPasswordField();
+        labelBoton = new javax.swing.JPanel();
+        contentBoton = new javax.swing.JLabel();
         jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setLocationByPlatform(true);
+        setUndecorated(true);
+        setResizable(false);
 
         background.setBackground(new java.awt.Color(255, 255, 255));
+        background.setBorder(javax.swing.BorderFactory.createEmptyBorder(1, 1, 1, 1));
         background.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        background.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(634, 0, 210, 500));
+
+        nombreMarket.setFont(new java.awt.Font("Roboto Black", 3, 24)); // NOI18N
+        nombreMarket.setForeground(new java.awt.Color(255, 204, 51));
+        nombreMarket.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        nombreMarket.setText("SuperMarket Nibble");
+        background.add(nombreMarket, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 270, 260, 80));
+
+        imgLogin.setIcon(new javax.swing.ImageIcon("C:\\Users\\dial_\\Documents\\NetBeansProjects\\supermarket\\src\\main\\java\\com\\nibble\\supermarket\\img\\login.jpg")); // NOI18N
+        background.add(imgLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 0, 310, 510));
+
+        nibble.setFont(new java.awt.Font("Roboto Black", 1, 18)); // NOI18N
+        nibble.setText("SUPERMARKET NIBBLE");
+        background.add(nibble, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 60, 230, 40));
+
+        loguito.setIcon(new javax.swing.ImageIcon(getClass().getResource("/logo.png"))); // NOI18N
+        background.add(loguito, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 70, -1, -1));
+
+        iniSesion.setFont(new java.awt.Font("Roboto Black", 1, 24)); // NOI18N
+        iniSesion.setText("INICIAR SESIÓN");
+        background.add(iniSesion, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 140, -1, -1));
+
+        contra.setFont(new java.awt.Font("Roboto", 1, 18)); // NOI18N
+        contra.setText("Contraseña");
+        background.add(contra, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 310, -1, -1));
+        background.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 380, 300, 40));
+
+        usuu.setFont(new java.awt.Font("Roboto", 1, 18)); // NOI18N
+        usuu.setText("Usuario");
+        background.add(usuu, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 190, -1, -1));
+
+        ingresoUsu1.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+        ingresoUsu1.setForeground(new java.awt.Color(255, 204, 153));
+        ingresoUsu1.setText("Ingrese su número de DNI");
+        ingresoUsu1.setToolTipText("");
+        ingresoUsu1.setBorder(null);
+        ingresoUsu1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ingresoUsu1ActionPerformed(evt);
+            }
+        });
+        background.add(ingresoUsu1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 230, 280, 30));
+        background.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 260, 300, 40));
+
+        jPasswordField1.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
+        jPasswordField1.setForeground(new java.awt.Color(255, 204, 153));
+        jPasswordField1.setText("jPasswordField1");
+        jPasswordField1.setBorder(null);
+        jPasswordField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jPasswordField1ActionPerformed(evt);
+            }
+        });
+        background.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 350, 250, 30));
+
+        labelBoton.setBackground(new java.awt.Color(249, 135, 0));
+        labelBoton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        contentBoton.setFont(new java.awt.Font("Roboto Black", 1, 14)); // NOI18N
+        contentBoton.setForeground(new java.awt.Color(255, 255, 255));
+        contentBoton.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        contentBoton.setText("Iniciar sesión");
+        contentBoton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+
+        javax.swing.GroupLayout labelBotonLayout = new javax.swing.GroupLayout(labelBoton);
+        labelBoton.setLayout(labelBotonLayout);
+        labelBotonLayout.setHorizontalGroup(
+            labelBotonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(contentBoton, javax.swing.GroupLayout.DEFAULT_SIZE, 140, Short.MAX_VALUE)
+        );
+        labelBotonLayout.setVerticalGroup(
+            labelBotonLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(contentBoton, javax.swing.GroupLayout.DEFAULT_SIZE, 40, Short.MAX_VALUE)
+        );
+
+        background.add(labelBoton, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 420, 140, 40));
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 40, Short.MAX_VALUE)
+            .addGap(0, 810, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
+            .addGap(0, 30, Short.MAX_VALUE)
         );
 
-        background.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 40, 100));
+        background.add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 810, 30));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(background, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(background, javax.swing.GroupLayout.PREFERRED_SIZE, 506, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 6, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void ingresoUsu1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ingresoUsu1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_ingresoUsu1ActionPerformed
+
+    private void jPasswordField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPasswordField1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -98,7 +200,19 @@ public class Login extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel background;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel contentBoton;
+    private javax.swing.JLabel contra;
+    private javax.swing.JLabel imgLogin;
+    private javax.swing.JTextField ingresoUsu1;
+    private javax.swing.JLabel iniSesion;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JPanel labelBoton;
+    private javax.swing.JLabel loguito;
+    private javax.swing.JLabel nibble;
+    private javax.swing.JLabel nombreMarket;
+    private javax.swing.JLabel usuu;
     // End of variables declaration//GEN-END:variables
 }
